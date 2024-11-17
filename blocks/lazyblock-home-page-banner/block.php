c501fe932cb6f5bbffd8497ac0154eec6cf5d209
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-11-17 17:25:29 +0000

    fix: get right size image for each screen

--- blocks/lazyblock-home-page-banner/block.php
+++ blocks/lazyblock-home-page-banner/block.php
@@ -1,15 +1,25 @@
 <section class="home__hero">
   <div class="home__hero-wrapper">
     <div class="home__hero-text-wrapper">
-      <h1 class="home__hero-heading"><?php echo esc_html($attributes['banner-heading']) ?></h1>
-      <h2 class="home__hero-sub-heading"><?php echo esc_html($attributes['banner-subheading']) ?></h2>
-      <a href="./contact/" class="home__hero-link">Book Now</a>
+      <h1 class="home__hero-heading"><?php echo esc_html($attributes['banner-heading']); ?></h1>
+      <h2 class="home__hero-sub-heading"><?php echo esc_html($attributes['banner-subheading']); ?></h2>
+      <a href="http://labellatheme.local/contact/" class="home__hero-link">Book Now</a>
     </div>
     <div class="home__hero-img-wrapper">
-      <?php if (isset($attributes['banner-img']['url'])): ?>
-        <img class="home__hero-img" src="<?php echo esc_url($attributes['banner-img']['url']); ?>"
-          alt="<?php echo esc_attr($attributes['control_name']['alt']); ?>">
-      <?php endif; ?>
+      <?php
+      if (isset($attributes['banner-img']['id'])) {
+        echo wp_get_attachment_image(
+          $attributes['banner-img']['id'],
+          'full',
+          false,
+          array(
+            'class' => 'home__hero-img',
+            'alt' => esc_attr($attributes['banner-img']['alt']),
+            'sizes' => '(max-width: 768px) 100vw, 50vw',
+          )
+        );
+      }
+      ?>
     </div>
   </div>
 </section>
