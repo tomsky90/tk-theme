8969f17c98665d895e76f0786db4348774249d09
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-12-02 07:45:55 +0000

    feat: home page banner for desktop

--- blocks/lazyblock-banner/block.php
+++ blocks/lazyblock-banner/block.php
@@ -1,13 +1,14 @@
+<section class="banner">
+  <div class="banner__text-wrapper">
+    <h1 class="banner__heading"><?php echo esc_html($attributes['banner-heading']) ?></h1>
+    <h2 class="banner__subheading"><?php echo esc_html($attributes['banner-subheading']) ?></h2>
+    <a href="<?php echo esc_url($attributes['banner-cta']); ?>" class="banner__btn">Get a Quote</a>
+  </div>
+  <div class="banner__img-wrapper">
 
-    <section class="banner">
-      <div class="banner__text-wrapper">
-        <h1 class="banner__heading"><?php echo esc_html($attributes['banner-heading'])?></h1>
-        <h2 class="banner__subheading"><?php echo esc_html($attributes['banner-subheading'])?></h2>
-      </div>
-      <div class="banner__img-wrapper">
-        <div class="banner__overlay"></div>
-        <?php if(isset($attributes['banner-img']['url'])) : ?>
-          <img class='banner__bgc-image' src="<?php echo esc_url($attributes['banner-img']['url'])?>" alt="<?php echo esc_attr($attributes['banner-img']['alt'])?>"/>
-        <?php endif?>
-      </div>
-    </section>
+    <?php if (isset($attributes['banner-img']['url'])): ?>
+      <img class='banner__bgc-image' src="<?php echo esc_url($attributes['banner-img']['url']) ?>"
+        alt="<?php echo esc_attr($attributes['banner-img']['alt']) ?>" />
+    <?php endif ?>
+  </div>
+</section>
