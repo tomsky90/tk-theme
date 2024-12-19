5c2e8a12298281faec9f9dfe3a4f8b2d8ddaaa3e
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-12-19 06:20:07 +0000

    feat: add cta section

---
+++ blocks/lazyblock-cta/block.php
@@ -0,0 +1,4 @@
+<section class="cta">
+  <h2 class="cta__heading"><?php echo esc_html($attributes['cta-heading']) ?></h2>
+  <a target="blank" href="<?php echo esc_url($attributes['cta-link']) ?>" class="cta__link cta-link">Get Quote</a>
+</section>
