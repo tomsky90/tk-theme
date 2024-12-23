96bd0e89a888c567e70f3956e6c8c9851fdd54b3
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-12-23 06:54:47 +0000

    feat: contact page styles

--- blocks/lazyblock-contact/block.php
+++ blocks/lazyblock-contact/block.php
@@ -38,9 +38,8 @@
 
   </div>
   <div class="contact__map-wrapper">
-    <iframe class="contact__map" title="Google Map" loading="lazy" allowFullScreen
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2379.745116344609!2d-2.2367841847992817!3d53.47769997999268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487bb1e6f4f53c3f%3A0xc001aa83b620c90!2sManchester%20M1!5e0!3m2!1sen!2suk!4v1620672014935!5m2!1sen!2suk"
-      style="border: 0;" aria-hidden="false" tabIndex="0">
-    </iframe>
+    <iframe class="contact__map"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19061.278550285777!2d-2.9986166106906356!3d53.331444004799536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487ad8cf8fd355eb%3A0x9bb55bbd9f9556cc!2sBromborough%2C%20Wirral!5e0!3m2!1sen!2suk!4v1734936050258!5m2!1sen!2suk"
+      loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </div>
 </section>
