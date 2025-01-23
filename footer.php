27eb10265e4b4d418821e7144af058e08ea5c1b2
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2025-01-23 14:45:37 +0000

    feat: footer

--- footer.php
+++ footer.php
@@ -6,62 +6,44 @@
     <div class="footer__links-wrapper">
       <a href="./home">
         <img loading="lazy" class="footer__logo"
-          src="<?php echo get_template_directory_uri() ?>/assets/images/logo.webp" alt="footer logo" />
+          src="<?php echo get_template_directory_uri() ?>/assets/images/Logo.webp" alt="footer logo" />
       </a>
     </div>
     <div class="footer__links-wrapper">
       <h3 class="footer__heading">Contact</h3>
 
-      <div class="footer__link-wrapper">
 
-        <img class="footer__link-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/Location-on.webp"
-          alt="map pin" />
-        <p class="footer__link">Wirral Ch62</p>
-      </div>
-      <div class="footer__link-wrapper">
-        <img class="footer__link-icon" src="<?php echo get_template_directory_uri() ?>/assets/images/Local-phone.webp"
-          alt="phone" />
-        <a class="footer__link" href="tel:078645723">
-          078645723
-        </a>
-      </div>
+
       <div class="footer__link-wrapper">
         <img class="footer__link-icon" src="<?php echo get_template_directory_uri() ?>/assets/images/Email.webp"
           alt="mail" />
-        <a class="footer__link" href="mailto:hi@wirrallandscaping.com">
-          hi@wirrallandscaping.com
+        <a class="footer__link" href="mailto:hello@tk-webdev.com">
+          hello@tk-webdev.com
         </a>
       </div>
     </div>
     <div class="footer__links-wrapper">
-      <h3 class="footer__heading">Socials</h3>
+      <h3 class="footer__heading">About</h3>
       <div class="footer__link-wrapper">
-        <img class="footer__link-icon" src="<?php echo get_template_directory_uri() ?>/assets/images/Facebook.webp"
-          alt="facebook" />
-        <a class="footer__link" href="www.instagram">
-          wirrallandscaping facebook
-        </a>
+        I’m a freelance web designer based on the Wirral, specializing in creating professional websites.
       </div>
-      <div class="footer__link-wrapper">
-        <img class="footer__link-icon" src="<?php echo get_template_directory_uri() ?>/assets/images/TwitterX.webp"
-          alt="x" />
-        <a class="footer__link" href="www.x.com">
-          wirrallandscaping-twitter
-        </a>
-      </div>
-    </div>
-    <div class="footer__links-wrapper">
-      <h3 class="footer__heading">We Are Open</h3>
-      <div class="footer__link-wrapper">
-        <p class="footer__link">Monday - Friday: 8am - 4pm</p>
-      </div>
-      <div class="footer__link-wrapper">
 
-        <p class="footer__link">Saturday: 8am - 12pm</p>
-      </div>
+    </div>
+    <div class="footer__links-wrapper">
+      <h3 class="footer__heading">Links</h3>
+      <ul class="footer__nav">
+        <li class='footer__link-wrapper'><a class="footer__link" href="#home">Home</a></li>
+        <li class='footer__link-wrapper'><a class="footer__link" href="#about">About</a></li>
+        <li class='footer__link-wrapper'><a class="footer__link" href="#services">Services</a></li>
+        <li class='footer__link-wrapper'><a class="footer__link" href="#prices">Prices</a></li>
+        <li class='footer__link-wrapper'><a class="footer__link" href="#portfolio">Portfolio</a></li>
+        <li class='footer__link-wrapper'><a class="footer__link" href="#contact">Contact</a></li>
+
+      </ul>
     </div>
 
   </div>
+  <p class="footer__copyright">&copy; 2025 All rights reserved</p>
 </footer>
 
 
